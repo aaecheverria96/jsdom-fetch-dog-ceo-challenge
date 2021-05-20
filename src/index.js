668b7779d1d0286fe5579f43eb73a7002a5c63b6
Your Username <you@example.com>
2021-05-20 17:06:50 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
-console.log('%c HI', 'color: firebrick')
+document.addEventListener('DOMContentLoaded', imageFetch)  
+
+function imageFetch() { 
+    fetch("https://dog.ceo/api/breeds/image/random/4") 
+    .then(function(resp){ 
+      
+       return resp.json() 
+    }) 
+    .then(function(json){ 
+        renderPictures(json.message) 
+    })
+}
+
+function renderPictures(array) { 
+    array.forEach(renderPicture)
+} 
+
+function renderPicture(pictureUrl) {  
+    const image = document.createElement('img') 
+    image.src = pictureUrl 
+    document.querySelector("#dog-image-container").appendChild(image)
+
+}
